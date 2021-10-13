c6acbb1e0900725fd27bc7de02b10901400ae1d1
kadowning1 <kadowning1@gmail.com>
2021-10-12 21:02:07 -0400

    update

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,37 @@
 const axios = require('axios');
-const apiKey = https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com
+const apiKey = https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com/api/menu/items/42
 
+class Section extends Component {
+    constructor(props) {
+        super(props)
+    }
+}
 
+componentDidUpdate()
+{
+    console.log('update component')
+}
+
+componentDidMount()
+{
+    console.log('regulators mount up')
+}
 
 // Make a request for a user with a given ID
 axios.get(apiKey)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
+
+render()
+{
+    console.log('render started')
+}
